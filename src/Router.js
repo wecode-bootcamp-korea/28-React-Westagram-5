f7d2c81a87756f42d02994ff8bd56a3dbc078d51
okch0310 <okch0310@naver.com>
2021-12-15 15:43:47 +0900

    Add: first commit. 초기 세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// 안신's 컴포넌트
+import LoginAnsin from './pages/ansin/Login/Login';
+import MainAnsin from './pages/ansin/Main/Main';
+
+// 선주's 컴포넌트
+import LoginSunjoo from './pages/sunjoo/Login/Login';
+import MainSunjoo from './pages/sunjoo/Main/Main';
+
+// 채현's 컴포넌트
+import LoginCheayeon from './pages/cheayeon/Login/Login';
+import MainCheayeon from './pages/cheayeon/Main/Main';
+
+// 석호's 컴포넌트
+import LoginSeokho from './pages/seokho/Login/Login';
+import MainSeokho from './pages/seokho/Main/Main';
+
+// 성용's 컴포넌트
+import LoginSeongyong from './pages/seongyong/Login/Login';
+import MainSeongyong from './pages/seongyong/Main/Main';
+
+// import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-ansin" element={<LoginAnsin />} />
+        <Route path="/main-ansin" element={<MainAnsin />} />
+        <Route path="/login-sunjoo" element={<LoginSunjoo />} />
+        <Route path="/main-sunjoo" element={<MainSunjoo />} />
+        <Route path="/login-cheayeon" element={<LoginCheayeon />} />
+        <Route path="/main-cheayeon" element={<MainCheayeon />} />
+        <Route path="/login-seokho" element={<LoginSeokho />} />
+        <Route path="/main-seokho" element={<MainSeokho />} />
+        <Route path="/login-seongyong" element={<LoginSeongyong />} />
+        <Route path="/main-seongyong" element={<MainSeongyong />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
