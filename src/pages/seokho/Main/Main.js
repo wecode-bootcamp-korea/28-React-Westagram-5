536a5d0d31182ee7506b889887df3d926e710a94
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2021-12-16 17:29:39 +0900

    Modify: Main pages space adjust & Navbar fixed

--- src/pages/seokho/Main/Main.js
+++ src/pages/seokho/Main/Main.js
@@ -7,19 +7,21 @@ import '@fortawesome/fontawesome-free/js/all.js';
 const Main = () => {
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <i className="fab fa-instagram" />
-          <div> </div>
-          <span> Westagram</span>
-        </div>
-        <input type="text" placeholder="검색" id="navbar-search" />
-        <div className="navbar-icons">
-          <img src="images/seokho/explore.png" alt="explore" />
-          <img src="images/seokho/heart.png" alt="heart" />
-          <img src="images/seokho/profile.png" alt="profile" />
-        </div>
-      </nav>
+      <div className="navbarFixed">
+        <nav className="navbar">
+          <div className="navbar-logo">
+            <i className="fab fa-instagram" />
+            <div> </div>
+            <span> Westagram</span>
+          </div>
+          <input type="text" placeholder="검색" id="navbar-search" />
+          <div className="navbar-icons">
+            <img src="images/seokho/explore.png" alt="explore" />
+            <img src="images/seokho/heart.png" alt="heart" />
+            <img src="images/seokho/profile.png" alt="profile" />
+          </div>
+        </nav>
+      </div>
       <main className="container">
         <aricle className="feeds">
           <div className="feeds-profile">
