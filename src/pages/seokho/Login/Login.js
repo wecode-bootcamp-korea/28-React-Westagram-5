9b7f751fa9710494eba35a8b02cac4839b6879bd
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2021-12-17 15:43:31 +0900

    Add: mission 1 complete

--- src/pages/seokho/Login/Login.js
+++ src/pages/seokho/Login/Login.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
+import { useState } from 'react';
+
+// event.target.value
 
 const Login = () => {
   const navigate = useNavigate();
-
   const goTomain = () => {
     navigate('/main-seokho');
   };
+
+  const [text, setText] = useState();
+
+  function handleIdInput(event) {
+    setText(event.target.value);
+    console.log(event.target.value);
+  }
 
   return (
     <div className="mainContainer">
@@ -15,11 +23,17 @@ const Login = () => {
         <h1>Westagram</h1>
         <div className="login">
           <input
+            onChange={handleIdInput}
             type="text"
             id="login-id"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
-          <input type="password" id="login-pw" placeholder="비밀번호" />
+          <input
+            onChange={handleIdInput}
+            type="password"
+            id="login-pw"
+            placeholder="비밀번호"
+          />
           <button onClick={goTomain} id="login-btn">
             {' '}
             로그인{' '}
