443aa92fda32f13dec0505353d39dca3cd8606fb
Cullen6732 <Cullen6732@gmail.com>
2021-12-17 16:46:50 +0900

    Test Commit

--- src/pages/seongyong/Login/Login.js
+++ src/pages/seongyong/Login/Login.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main-seongyong');
+  };
+
+  return (
+    <div className="bigcontainer">
+      <div className="container">
+        <div className="logo">Westagram</div>
+        <div className="warp loginform">
+          <div className="loginId">
+            <input
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              id="id"
+            />
+          </div>
+          <div className="loginPw">
+            <input type="text" placeholder="비밀번호" id="pw" />
+          </div>
+          <button onClick={goToMain} id="loginBtn" type="button">
+            로그인
+          </button>
+          <a href="https://www.instagram.com/accounts/password/reset/">
+            <footer>비밀번호를 잊으셨나요?</footer>
+          </a>
+        </div>
+      </div>
+      <div className="bottom">Copyright by SeongYong</div>
+    </div>
+  );
 };
+
 export default Login;
